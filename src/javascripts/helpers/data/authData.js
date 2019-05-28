583448a90338655800c21be0d198293266a25bc2
Mark Okafor <okaformark@gmail.com>
2019-05-28 18:38:35 -0500

    made links into button

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -5,6 +5,9 @@ const authDiv = document.getElementById('auth');
 const authNavBar = document.getElementById('navbar-button-auth');
 const logOutNavBar = document.getElementById('navbar-button-logout');
 const myMovieNavBar = document.getElementById('navbar-button-movieHistory');
+const myMovieButton = document.getElementById('myMovieBtn');
+const logOutButton = document.getElementById('logOutBtn');
+const authButton = document.getElementById('authBtn');
 
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
@@ -14,11 +17,17 @@ const checkLoginStatus = () => {
       authNavBar.classList.add('hide');
       myMovieNavBar.classList.remove('hide');
       logOutNavBar.classList.remove('hide');
+      myMovieButton.classList.remove('hide');
+      logOutButton.classList.remove('hide');
+      authButton.classList.add('hide');
     } else {
       authNavBar.classList.remove('hide');
       logOutNavBar.classList.add('hide');
       authDiv.classList.remove('hide');
       myMovieNavBar.classList.add('hide');
+      logOutButton.classList.add('hide');
+      myMovieButton.classList.add('hide');
+      authButton.classList.remove('hide');
     }
   });
 };
